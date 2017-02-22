f16c52d73b73f92a9be89c95c7bd72a2fd358930
Swapnil Haria <swapnilh@pratchett.cs.wisc.edu>
2017-02-21 19:02:12 -0600

    Adding the install script

---
+++ install.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import os
+from subprocess import Popen, PIPE
+
+workload = 'nvml'
+
+def runCmd(cmd, err, out):
+    """
+    Takes two strings, command and error, runs it in the shell
+    and then if error string is found in stdout, exits.
+    For no output = no error, use err=""
+    """
+    print cmd
+    (stdout, stderr) = Popen(cmd, shell=True, stdout=PIPE).communicate()
+    if err is None:
+        if stdout != "":
+            print "Error: %s" %(out,)
+            print "Truncated stdout below:"
+            print '... ', stdout[-500:]
+            sys.exit(2)
+    else:
+        if err in stdout:
+            print "Error: %s" %(out,)
+            print "Truncated stdout below:"
+            print '... ', stdout[-500:]
+            sys.exit(2)
+
+def main(argv): 
+    """
+    Parses the arguments and cleans and/or builds the specified
+    workloads of the whisper suite
+    """
+    parser = argparse.ArgumentParser(description='Builds echo from'
+        'the whisper suite.')
+    parser.add_argument('--clean', dest='clean', action='store_true',
+                default='false',
+                help='clean')
+    parser.add_argument('--build', dest='build', action='store_true',
+                default='false',
+                help='build')
+
+    args = parser.parse_args()
+    print('UPDATE linker map for making your code visible deps/nvml'
+        '/src/libpmemobj/libpmemobj.map else compilation fails')
+    if args.clean == True:
+        print "Cleaning " + workload
+        runCmd("make distclean", "No rule", "Couldn't clean %s dir!" % (workload, ))
+        runCmd("make clean", "No rule", "Couldn't clean %s dir!" % (workload, ))
+        runCmd("bash ./src/cleanup.sh", "Error", "Couldn't clean %s dir!" % (workload, ))
+
+    if args.build == True:
+        print "Building " + workload
+        buildCmd = 'make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99'\
+        ' EXTRA_CFLAGS="-Wno-error"'
+        runCmd(buildCmd, "Error", "Couldn't build %s" % (workload,))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
