54871035ad2a8a59f26dd00f02287aecf19d5b01
Sanketh Nalli <nalli@wisc.edu>
2017-03-04 23:43:04 -0600

    fix run_*.sh

--- run_ctree.sh
+++ run_ctree.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+action=$1
+trace=$2
+if [[ $trace == '--trace' ]]
+then
+        trace='y'
+fi
 
-export PMEM_TRACE_ENABLE=y     # y or n
+export PMEM_TRACE_ENABLE=$trace     # y or n
 export PMEM_NO_MOVNT=1
 export PMEM_MMAP_HINT=0x0000100000000000
 export PMEM_IS_PMEM_FORCE=1
@@ -14,8 +20,17 @@ bin="./src/benchmarks/pmembench map_insert"
 if [[ $action == '-h' ]]
 then
 	$bin -h
+elif [[ $action == '--small' ]]
+then
+	$bin -f $dir -d 128 -n 1024 -t 2 -r 1 -T ctree   
+elif [[ $action == '--med' ]]
+then
+	$bin -f $dir -d 128 -n 10240 -t 2 -r 2 -T ctree   
+elif [[ $action == '--large' ]]
+then
+	$bin -f $dir -d 128 -n 102400 -t 4 -r 2 -T ctree   
 else
-	$bin -f $dir -d 128 -n 102400 -t 4 -r 1 -T ctree   
+	exit
 fi
 #strace -f -e trace=write ./src/benchmarks/pmembench map_insert -f $dir -d 128 -n 1 -t 1 -r 1 -T ctree   
 #gdb --args ./src/benchmarks/pmembench map_insert -f $dir -d 128 -n 102400 -t 4 -r 1 -T ctree   
