6fe1d6b9b4863c0a8ac2339c0d09b85c8ddfcefc
winterNan <yuanyao@kth.se>
2021-08-18 22:41:50 +0200

    Ported to Gem5

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #make distclean
-#make clean
 #./src/cleanup.sh
 
-#make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_UTRACE -Wno-error" 
-make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_FTRACE -Wno-error" 
+make clean
+make all STATIC_LINK_PMEMBENCH=yes NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_UTRACE -Wno-error" 
+# make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_FTRACE -Wno-error -static -static-libgcc" EXTRA_LDFLAGS="-static"
 #make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-Wno-error" 
